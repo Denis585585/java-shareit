4b0851958f49093cb21b39d1e26b264d2f446b3b
Denis585585 <Vegan585@yandex.ru>
2025-03-25 00:14:46 +0300

    add-requests правка тестов (правка чек стайла)

--- server/src/main/java/ru/practicum/shareit/booking/dao/BookingService.java
+++ server/src/main/java/ru/practicum/shareit/booking/dao/BookingService.java
@@ -4,7 +4,6 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingNewDto;
 import ru.practicum.shareit.booking.util.BookingState;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface BookingService {
