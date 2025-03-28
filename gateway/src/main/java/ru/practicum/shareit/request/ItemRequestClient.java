de641defe16c684656b44c5bf78abb43c62c9c7f
Denis585585 <Vegan585@yandex.ru>
2025-03-28 19:20:47 +0300

    add-requests правки по чекстайлу

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
